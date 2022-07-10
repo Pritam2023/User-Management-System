479ddb1d7739a65b492b76a2b5615be301419c56
Pritam Chatterjee <74919879+Pritam2023@users.noreply.github.com>
2022-07-10 23:22:38 +0530

    Update model.js

--- server/model/model.js
+++ server/model/model.js
@@ -10,6 +10,11 @@ var schema = new mongoose.Schema({
         required: true,
         unique: true
     },
+    mobile : {
+        type: String,
+        required: true,
+        unique: true
+    },
     gender : String,
     status : String
 })
